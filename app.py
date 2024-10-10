a49fba7b96cafbef49ee1e2c8fb11e11736c2503
Jasmine Neo <jasmine@Jasmines-MacBook-Air.local>
2024-10-10 10:30:48 -0500

    first commit

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from __future__ import unicode_literals
 from flask import Flask, request, send_file, render_template, redirect, flash, send_from_directory
-app = Flask(__name__)
-app.secret_key = "your_secret_key"
-from pytube import YouTube
+from pytubefix import YouTube  # Import YouTube from pytubefix
+from pytubefix.cli import on_progress  # Import on_progress callback from pytubefix
 import os
 import yt_dlp
-# import youtube_dl
+
+app = Flask(__name__)
+app.secret_key = "neo"
 
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/terms-conditions')
 def terms():
-	return render_template('terms-conditions.html')
+    return render_template('terms-conditions.html')
+
 
 @app.route('/download', methods=["POST", "GET"])
 def download_mp3():
@@ -27,8 +31,8 @@ def download_mp3():
         flash('Please enter a valid URL.')
         return render_template('index.html')
     try:
-        # try with pytube first
-        yt = YouTube(url)
+        # Try with pytubefix first
+        yt = YouTube(url, on_progress_callback=on_progress)
         video = yt.streams.filter(only_audio=True).first()
         out_file = video.download()
         base, ext = os.path.splitext(out_file)
@@ -37,8 +41,8 @@ def download_mp3():
         return send_file(new_file, as_attachment=True)
 
     except Exception as e:
-        # if pytube fails, use yt-dlp
-        print("Pytube encountered an error, falling back to yt-dlp: ", str(e))
+        # If pytube (pytubefix) fails, use yt-dlp
+        print("Pytubefix encountered an error, falling back to yt-dlp: ", str(e))
         try:
             ydl_opts = {
                 'outtmpl': 'downloads/%(title)s.%(ext)s',
@@ -60,8 +64,6 @@ def download_mp3():
             return render_template('index.html')
 
 
-
-
 @app.route('/downloadvid', methods=["POST", "GET"])
 def download_video():
     try:
@@ -74,7 +76,7 @@ def download_video():
             'outtmpl': 'downloads/%(title)s.%(ext)s',
             'format': 'bestvideo[ext=mp4][vcodec=avc1]+bestaudio[ext=m4a]/mp4+best[height<=480]'
         }
-        
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
             filename = ydl.prepare_filename(info_dict)
@@ -85,29 +87,6 @@ def download_video():
 
 
 if __name__ == '__main__':
-	app.run(port=5000, debug=True)
+    app.run(debug=True)
 
-
-
-
-
-########### pytube version mp3 audio ##############
-# @app.route('/download', methods=["POST", "GET"])
-# def download_mp3():
-#     if request.method == 'POST':
-#         url = request.form.get("url", None)
-#         if not url:
-#             flash('Please enter a valid URL.')
-#             return render_template('index.html')
-#         try:
-#             yt = YouTube(url)
-#             video = yt.streams.filter(only_audio=True).first()
-#             out_file = video.download()
-#             base, ext = os.path.splitext(out_file)
-#             new_file = base + '.mp3'
-#             os.rename(out_file, new_file)
-#             return send_file(new_file,as_attachment=True)
-#         except Exception as e:
-#             flash(str(e))
-#             return render_template('index.html')
-#     return render_template('index.html')
+    #app.run(port=5000, debug=True)
